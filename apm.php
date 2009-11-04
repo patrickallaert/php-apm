01434a619b17a4c4424bdbf92a4d78dbe63ee2dd
patrickallaert <patrickallaert@c90b9560-bf6c-de11-be94-00142212c4b1>
2009-11-04 19:28:23 +0000

    Added: styling of the monitoring page by Jehan Bihin (http://www.daaboo.net/)
    git-svn-id: https://svn.php.net/repository/pecl/apm/trunk@292984 c90b9560-bf6c-de11-be94-00142212c4b1

--- apm.php
+++ apm.php
@@ -3,6 +3,63 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
 <title>APM status</title>
+<style type="text/css">
+<!--
+/* Design by Jehan Bihin (http://www.daaboo.net/) */
+  * {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  body {
+    font-size: 62.5%;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+  h1 {
+    background-color: #9999CC;
+    font-size: 2em;
+    padding: 0.2em 0.5em;
+  }
+  h2 {
+    background-color: #eee;
+    border-color: #999;
+    border-style: solid;
+    border-width: 1px 0;
+    padding: 0.2em 0.6em;
+    font-size: 1.4em;
+  }
+  table {
+    margin: 1em 1em 1.6em;
+    font-size: 1.2em;
+    border-collapse: collapse;
+  }
+  table th {
+    padding: 0 0.5em;
+    text-align: left;
+    border-bottom: 1px solid #999;
+  }
+  table td {
+    padding: 0.2em 0.5em;
+  }
+  table#slow-request-list td { border-bottom: 1px solid #ccc; }
+  table#slow-request-list tr.odd td { background-color: #eee; }
+  table#event-list tr.E_ERROR td,
+  table#event-list tr.E_CORE_ERROR td,
+  table#event-list tr.E_PARSE td,
+  table#event-list tr.E_COMPILE_ERROR td,
+  table#event-list tr.E_USER_ERROR td,
+  table#event-list tr.E_RECOVERABLE_ERROR td { background-color: #fbc; }
+  table#event-list tr.E_WARNING td,
+  table#event-list tr.E_CORE_WARNING td,
+  table#event-list tr.E_COMPILE_WARNING td,
+  table#event-list tr.E_USER_WARNING td { background-color: #fc8; }
+  table#event-list tr.E_NOTICE td,
+  table#event-list tr.E_USER_NOTICE td,
+  table#event-list tr.E_STRICT td,
+  table#event-list tr.E_DEPRECATED td,
+  table#event-list tr.E_USER_DEPRECATED td { background-color: #ff8; }
+-->
+</style>
 </head>
 <body>
 <h1>APM status</h1>
