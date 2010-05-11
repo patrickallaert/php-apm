68c8274f6a8eb307efa7a4fca2d2890c1911d7f8
patrickallaert <patrickallaert@c90b9560-bf6c-de11-be94-00142212c4b1>
2010-05-11 07:33:24 +0000

    Fixed: apm.mysql_error_reporting not set to E_ALL by default
    git-svn-id: https://svn.php.net/repository/pecl/apm/trunk@299235 c90b9560-bf6c-de11-be94-00142212c4b1

--- drivers/mysql.c
+++ drivers/mysql.c
@@ -42,7 +42,7 @@ PHP_INI_BEGIN()
  	/* Boolean controlling whether the driver is active or not */
  	STD_PHP_INI_BOOLEAN("apm.mysql_enabled",       "1",               PHP_INI_PERDIR, OnUpdateBool,   enabled,             zend_apm_mysql_globals, apm_mysql_globals)
 	/* error_reporting of the driver */
-	STD_PHP_INI_ENTRY("apm.mysql_error_reporting", NULL,         PHP_INI_ALL,    OnUpdateAPMmysqlErrorReporting,   error_reporting,     zend_apm_mysql_globals, apm_mysql_globals)
+	STD_PHP_INI_ENTRY("apm.mysql_error_reporting", "E_ALL",         PHP_INI_ALL,    OnUpdateAPMmysqlErrorReporting,   error_reporting,     zend_apm_mysql_globals, apm_mysql_globals)
 	/* mysql host */
 	STD_PHP_INI_ENTRY("apm.mysql_host",            "localhost",       PHP_INI_PERDIR, OnUpdateString, db_host,             zend_apm_mysql_globals, apm_mysql_globals)
 	/* mysql port */
