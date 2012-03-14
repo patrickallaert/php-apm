85eec80b4a8fb69c85ce353721e3248c5797d62e
patrickallaert <patrickallaert@c90b9560-bf6c-de11-be94-00142212c4b1>
2012-03-14 07:09:09 +0000

    Added: MySQL SQL create script
    git-svn-id: https://svn.php.net/repository/pecl/apm/trunk@324215 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ sql/mysql/create.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS event (
+    id INTEGER UNSIGNED PRIMARY KEY auto_increment,
+    ts TEXT NOT NULL,
+    type TINYINT UNSIGNED NOT NULL,
+    file TEXT NOT NULL,
+    line MEDIUMINT UNSIGNED NOT NULL,
+    message TEXT NOT NULL,
+    backtrace TEXT NOT NULL);
+
+CREATE TABLE IF NOT EXISTS slow_request (
+    id INTEGER UNSIGNED PRIMARY KEY auto_increment,
+    ts TEXT NOT NULL,
+    duration FLOAT NOT NULL,
+    file TEXT NOT NULL);
