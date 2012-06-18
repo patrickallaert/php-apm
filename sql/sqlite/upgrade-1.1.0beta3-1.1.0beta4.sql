ae6d4ff2517cfadb7a8ed2d23f869191d9514e9a
patrickallaert <patrickallaert@c90b9560-bf6c-de11-be94-00142212c4b1>
2012-06-18 21:54:13 +0000

    Added: missing upgrade script
    git-svn-id: https://svn.php.net/repository/pecl/apm/trunk@326224 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ sql/sqlite/upgrade-1.1.0beta3-1.1.0beta4.sql
@@ -0,0 +1,32 @@
+CREATE TABLE IF NOT EXISTS event2 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ts INTEGER NOT NULL,
+    type INTEGER NOT NULL,
+    file TEXT NOT NULL,
+    line INTEGER NOT NULL,
+    message TEXT NOT NULL,
+    backtrace TEXT NOT NULL,
+    uri TEXT NOT NULL,
+    host TEXT NOT NULL,
+    ip INTEGER UNSIGNED NOT NULL,
+    cookies TEXT NOT NULL,
+    post_vars TEXT NOT NULL,
+    referer TEXT NOT NULL
+);
+
+INSERT INTO event2 SELECT id, strftime('%s',ts), type, file, line, message, backtrace, '', '', 0, '', '', '' FROM event;
+
+DROP TABLE event;
+ALTER TABLE event2 RENAME TO event;
+
+CREATE TABLE IF NOT EXISTS slow_request2 (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    ts INTEGER NOT NULL,
+    duration FLOAT NOT NULL,
+    file TEXT NOT NULL
+);
+
+INSERT INTO slow_request2 SELECT id, strftime('%s',ts), duration, file FROM slow_request;
+
+DROP TABLE slow_request;
+ALTER TABLE slow_request2 RENAME TO slow_request;
